63577d8c3ec5f3d4dbc90b834a72bd5360ad2bd6
Amin Soltani <amin@aminsoltani.com>
2018-04-13 21:54:25 -0700

    Preparation

--- ttt_AI.py
+++ ttt_AI.py
@@ -120,8 +120,11 @@ class TTTClient:
         self.client_socket.close();
 
 
-class TTTClientGame(TTTClient):
+class AiPlayer(TTTClient):
     """TTTClientGame deals with the game logic on the client side."""
+    board_content = []
+    play_as = ''
+    command = ""
 
     def __init__(self):
         """Initializes the client game object."""
@@ -138,24 +141,23 @@ class TTTClientGame(TTTClient):
         self.__connected__();
 
         # Receive the assigned role from the server
-        self.role = str(self.s_recv(2, "R"));
+        self.role = str(self.s_recv(2, "R"))
         # Confirm the assigned role has been received
-        self.s_send("c", "2");
+        self.s_send("c", "2")
 
         # Receive the mactched player's ID from the server
-        self.match_id = int(self.s_recv(128, "I"));
+        self.match_id = int(self.s_recv(128, "I"))
         # Confirm the mactched player's ID has been received
-        self.s_send("c", "3");
+        self.s_send("c", "3")
 
         print(("You are now matched with player " + str(self.match_id)
-               + "\nYou are the \"" + self.role + "\""));
+               + "\nYou are the \"" + self.role + "\""))
 
         # Call the __game_started() function, which might be implemented by
         # the GUI program to interact with the user interface.
-        self.__game_started__();
-
+        self.__game_started__()
         # Start the main loop
-        self.__main_loop();
+        self.__main_loop()
 
     def __connected__(self):
         """(Private) This function is called when the client is successfully
@@ -174,33 +176,34 @@ class TTTClientGame(TTTClient):
         """The main game loop."""
         while True:
             # Get the board content from the server
-            board_content = self.s_recv(10, "B");
+            self.board_content = self.s_recv(10, "B")
             # Get the command from the server
-            command = self.s_recv(2, "C");
+            self.command = self.s_recv(2, "C");
             # Update the board
-            self.__update_board__(command, board_content);
 
-            if (command == "Y"):
+            self.__update_board__()
+            print("bbb")
+            if (self.command == "Y"):
                 # If it's this player's turn to move
-                self.__player_move__(board_content);
-            elif (command == "N"):
+                self.make_move(self.board_content);
+            elif (self.command == "N"):
                 # If the player needs to just wait
                 self.__player_wait__();
                 # Get the move the other player made from the server
                 move = self.s_recv(2, "I");
                 self.__opponent_move_made__(move);
-            elif (command == "D"):
+            elif (self.command == "D"):
                 # If the result is a draw
                 print("It's a draw.");
                 break;
-            elif (command == "W"):
+            elif (self.command == "W"):
                 # If this player wins
                 print("You WIN!");
                 # Draw winning path
                 self.__draw_winning_path__(self.s_recv(4, "P"));
                 # Break the loop and finish
                 break;
-            elif (command == "L"):
+            elif (self.command == "L"):
                 # If this player loses
                 print("You lose.");
                 # Draw winning path
@@ -214,48 +217,19 @@ class TTTClientGame(TTTClient):
                 # And finish
                 break;
 
-    def __update_board__(self, command, board_string):
+    def __update_board__(self):
         """(Private) Updates the board. This function might be overridden by
         the GUI program."""
-        if (command == "Y"):
+        if (self.command == "Y"):
             # If it's this player's turn to move, print out the current
             # board with " " converted to the corresponding position number
-            print("Current board:\n" + TTTClientGame.format_board(
-                TTTClientGame.show_board_pos(board_string)));
+
+            print("Current board:\n" + AiPlayer.format_board(
+                self.show_board_pos()))
         else:
             # Print out the current board
-            print("Current board:\n" + TTTClientGame.format_board(
-                board_string));
-
-    def __player_move__(self, board_string):
-        """(Private) Lets the user input the move and sends it back to the
-        server. This function might be overridden by the GUI program."""
-        while True:
-            # Prompt the user to enter a position
-            try:
-                position = int(input('Please enter the position (1~9):'));
-            except:
-                print("Invalid input.");
-                continue;
-
-            # Ensure user-input data is valid
-            if (position >= 1 and position <= 9):
-                # If the position is between 1 and 9
-                if (board_string[position - 1] != " "):
-                    # If the position is already been taken,
-                    # Print out a warning
-                    print("That position has already been taken." +
-                          "Please choose another one.");
-                else:
-                    # If the user input is valid, break the loop
-                    break;
-            else:
-                print("Please enter a value between 1 and 9 that" +
-                      "corresponds to the position on the grid board.");
-        # Loop until the user enters a valid value
-
-        # Send the position back to the server
-        self.s_send("i", str(position));
+            print("Current board:\n" + AiPlayer.format_board(
+                self.board_content))
 
     def __player_wait__(self):
         """(Private) Lets the user know it's waiting for the other player to
@@ -277,14 +251,13 @@ class TTTClientGame(TTTClient):
 
         print("The path is: " + readable_path[:-2]);
 
-    def show_board_pos(s):
+    def show_board_pos(self):
         """(Static) Converts the empty positions " " (a space) in the board
         string to its corresponding position index number."""
-
         new_s = list("123456789");
         for i in range(0, 8):
-            if (s[i] != " "):
-                new_s[i] = s[i];
+            if (self.board_content[i] != " "):
+                new_s[i] = self.board_content[i];
         return "".join(new_s);
 
     def format_board(s):
@@ -305,6 +278,66 @@ class TTTClientGame(TTTClient):
                 + "|" + s[3] + "|" + s[4] + "|" + s[5] + "|\n"
                 + "|" + s[6] + "|" + s[7] + "|" + s[8] + "|\n");
 
+    def make_move(self, position):
+        """(Private) Lets the user input the move and sends it back to the
+        server. This function might be overridden by the GUI program."""
+        print(self.board_content)
+        while True:
+            # Prompt the user to enter a position
+            try:
+                position = int(input('Please enter the position (1~9):'));
+            except:
+                print("Invalid input.");
+                continue;
+
+            # Ensure user-input data is valid
+            if (position >= 1 and position <= 9):
+                # If the position is between 1 and 9
+                if (self.board_content[position - 1] != " "):
+                    # If the position is already been taken,
+                    # Print out a warning
+                    print("That position has already been taken." +
+                          "Please choose another one.");
+                else:
+                    # If the user input is valid, break the loop
+                    break;
+            else:
+                print("Please enter a value between 1 and 9 that" +
+                      "corresponds to the position on the grid board.");
+        # Loop until the user enters a valid value
+
+        # Send the position back to the server
+        self.s_send("i", str(position));
+
+    def opponent_pos(self):
+        return []
+
+    def agent_pos(self):
+        return []
+
+    def is_game_over(self):
+        return False
+
+    def is_draw(self):
+        return False
+
+    def is_won(self):
+        return False
+
+    def play_as(self):
+        return 'X'
+
+    def opponent_last_move(self):
+        return False
+
+    def agent_last_move(self):
+        return False
+
+    def all_avaiable_pos(self):
+        return False
+
+    def position_score(position):
+        return 100
 
 # Define the main program
 def main():
@@ -318,21 +351,21 @@ def main():
         address = input("Please enter the address:");
         port_number = input("Please enter the port:");
 
-    # Initialize the client object
-    client = TTTClientGame();
+    # Initialize the agent object
+    agent = AiPlayer();
     # Connect to the server
-    client.connect(address, port_number);
+    agent.connect(address, port_number);
     try:
         # Start the game
-        client.start_game();
+        agent.start_game();
     except:
         print(("Game finished unexpectedly!"));
     finally:
-        # Close the client
-        client.close();
+        # Close the agent
+        agent.close();
 
 
 if __name__ == "__main__":
     # If this script is running as a standalone program,
     # start the main program.
-    main();
+    main()
