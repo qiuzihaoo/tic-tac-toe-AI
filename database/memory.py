dcc9265f6cdbd6065655dfa7728d63ded9e3353e
Amin Soltani <solamin.amin@gmail.com>
2018-04-20 01:17:20 -0700

    Registering each move of the agent in the database

--- database/memory.py
+++ database/memory.py
@@ -7,11 +7,15 @@ class Memory:
         self.conn = MySQLdb.connect(host="127.0.0.1", user="root", passwd="", db="tictactoe")
 
     def save(self, board_before, move, role, is_new):
-        db = self.conn.cursor()
+        query = self.conn.cursor()
         move = int(move)
-        board_before = board_after = board_before.replace(' ', '-')
-        board_after[move] = role
-        query = "INSERT INTO `shortterm` (`board_before`, `move`, `board_after`, `role`, `is_new`) VALUES ('%s', %d, '%s', '%s', %d)" % (board_before, move, board_after, role, is_new)
-        print(query)
-        db.execute(query)
-        self.conn.commit()
+        board_before = board_before.replace(' ', '-')
+        board_after = list(board_before)
+        board_after[move - 1] = role[0]
+        board_after = "".join(board_after)
+        statement = "INSERT INTO `shortterm` (`board_before`, `move`, `board_after`, `role`, `is_new`) " \
+                    "VALUES ('%s', %d, '%s', '%s', %d)" \
+                    % (board_before, move, board_after, role, is_new)
+        print(statement)
+        query.execute(statement)
+        return self.conn.commit()
